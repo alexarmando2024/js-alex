c4c6187385ece4fa66723978523cb1aafeec4104
alexarmando2024 <00001105067816sp@al.educacao.sp.gov.br>
2024-05-10 07:51:13 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400);
+    background("lightgrey")
+  
+    textFont("Verdana");
+    textSize(30)
+    textAlign(CENTER,CENTER);
+        text("Aprendendo JS na Alura",200,200)
+  
+  
+  
+  }
+  function draw(){
+    
+    textFont("Impact");
+    textSize(30)
+    textAlign(CENTER,CENTER);
+  
+      text("Estou amando o curso e ",200,170)
+      
+  }
+  
